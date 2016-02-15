19526850653809ada4e6cb966bcddc34d267bf3e
Felix Barangan <franb86.2@gmail.com>
2016-02-14 19:22:27 -0500

    Update creatingNewVariable.R

--- 3_GettingCleanData/RcodeFromLecture/creatingNewVariable.R
+++ 3_GettingCleanData/RcodeFromLecture/creatingNewVariable.R
@@ -1,1 +1,11 @@
-#Creating new Variable ?
+# Creating new Variable ?
+# Common values to create
+# missingness indicators
+#"Cutting up" quantitative variables
+#Applying transforms
+
+# Sample data  https://data.baltimorecity.gov/Community/Restaurants/k5ry=ef3g
+
+# Getting data from the web
+
+if(!file.exists)
