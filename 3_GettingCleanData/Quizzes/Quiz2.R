948c830f026a114978cc12434188e2d30a68e59d
Fbarangan <franb86.2@gmail.com>
2016-02-19 19:17:16 -0500

    initial commit

--- 3_GettingCleanData/Quizzes/Quiz2.R
+++ 3_GettingCleanData/Quizzes/Quiz2.R
@@ -1,1 +1,16 @@
 # Quiz 2
+# data : https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg
+install.packages("jpeg")
+
+library(jpeg)
+
+if(!file.exists("/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes"))
+{dir.create("/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes")}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+
+jpg <- download.file(fileUrl, destfile = "/Users/kfcbarangan/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes/jpg.jpg" , mode = "wb")
+
+img <- readJPEG("/Users/kfcbarangan/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes/jpg.jpg", native = TRUE)
+quantile(img, probs = c(0.3,0.8))
+
