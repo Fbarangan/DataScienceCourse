1e99189831f081bf027bf42a1e335575bd05530d
Fbarangan <franb86.2@gmail.com>
2016-02-21 17:42:50 -0500

    init commit

--- 3_GettingCleanData/Quizzes/Week4Quiz2.R
+++ 3_GettingCleanData/Quizzes/Week4Quiz2.R
@@ -1,3 +1,12 @@
 # Question 2
 # Data  "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 # Original data from  http://data.worldbank.org/data-catalog/GDP-ranking-table
+
+if(!file.exists("/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes"))
+{dir.create("/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes")}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+
+GDPRaw <- download.file(fileUrl, destfile = "/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes/GDPRaw.csv")
+
+GDP <- read.csv("/Users/~/Desktop/DataScienceCourse/3_GettingCleanData/Quizzes/GDPRaw.csv", stringsAsFactors = FALSE, header = FALSE)
