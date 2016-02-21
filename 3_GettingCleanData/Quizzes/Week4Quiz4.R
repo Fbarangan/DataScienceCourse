7548c27d66f647eb09f2c64fab6a11e5519ef7fb
Felix Barangan <franb86.2@gmail.com>
2016-02-21 14:59:17 -0500

    Update Week4Quiz4.R

--- 3_GettingCleanData/Quizzes/Week4Quiz4.R
+++ 3_GettingCleanData/Quizzes/Week4Quiz4.R
@@ -7,21 +7,3 @@
 #Original data source  http://data.worldbank.org/data-catalog/GDP-ranking-table
 #                       http://data.worldbank.org/data-catalog/ed-stats
 
-# Quantitative Financial Modelling & Trading Framework for R  http://www.quantmod.com/
-# Use Base R instead of Studio. It is saying it is curently not compatible with my R Studio Version
-
-install.packages("quantmod")
-library(quantmod)
-getwd()
-
-amzn <- getSymbols("AMZN", auto.assign = FALSE)
-str(sampleTimes)
-head(sampleTimes)
-
-summary(sampleTimes)
-formattedValues <- format(sampleTimes, "%a %Y")
-head(formattedValues)
-
-length(grep("2012", formattedValues))
-length(grep("Mon 2012", formattedValues))
-
