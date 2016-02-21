62104e3dd17f61d69cdf9ccade82e638aa654467
Felix Barangan <franb86.2@gmail.com>
2016-02-21 14:27:28 -0500

    Create Week4Quiz4.R

---
+++ 3_GettingCleanData/Quizzes/Week4Quiz4.R
@@ -0,0 +1,10 @@
+# Question 4
+
+# GDP Data source "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+
+# Educational Data set   "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+
+#Original data source  http://data.worldbank.org/data-catalog/GDP-ranking-table
+#                       http://data.worldbank.org/data-catalog/ed-stats
+
+# Quantitative Financial Modelling & Trading Framework for R  http://www.quantmod.com/
