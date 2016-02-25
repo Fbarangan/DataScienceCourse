5410d670bfb67415488d2567213fa9d5d0333545
Felix Barangan <franb86.2@gmail.com>
2016-02-25 12:49:27 -0500

    Update run_analysis.R

--- 3_GettingCleanData/Course_Project/run_analysis.R
+++ 3_GettingCleanData/Course_Project/run_analysis.R
@@ -2,3 +2,13 @@
 
 library(plyr)
 library(dplyr)
+
+# Create folder where the data will reside
+# Make sure working directory is set to the correct path
+if (!file.exists("paste you PATH here"))
+        {dir.create("paste you PATH here")}
+
+# File assignment
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+
