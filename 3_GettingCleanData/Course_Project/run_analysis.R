7af345cb2af0fd636e5d4359df21c8b6048134fd
Felix Barangan <franb86.2@gmail.com>
2016-02-25 13:19:26 -0500

    Update run_analysis.R

--- 3_GettingCleanData/Course_Project/run_analysis.R
+++ 3_GettingCleanData/Course_Project/run_analysis.R
@@ -4,11 +4,23 @@ library(plyr)
 library(dplyr)
 
 # Create folder where the data will reside
-# Make sure working directory is set to the correct path
+# Make sure working directory is set to the correct  file path
 if (!file.exists("paste you PATH here"))
         {dir.create("paste you PATH here")}
 
-# File assignment
+# File assignment. Mke sure to back to that file/folder and unzip file. see ReadMe file for details.
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+# read tables 
+#     test folder ( test folder :  X-test,  y_test, subject_test ). Make sure to plug in correct path of the files
+
+test <- read.table("/paste you PATH here/test/X_test.txt")
+test <- read.table("/paste you PATH here/test/Y_test.txt")
+testSubject <- read.table("/paste you PATH here/test/subject_test.txt")
+
+#     
+
+
+       
 
 
