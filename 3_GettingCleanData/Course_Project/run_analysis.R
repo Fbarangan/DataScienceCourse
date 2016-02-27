ec8b9d1727dc9606b355c195be346d2eb741f621
Felix Barangan <franb86.2@gmail.com>
2016-02-26 21:31:13 -0500

    Update run_analysis.R

--- 3_GettingCleanData/Course_Project/run_analysis.R
+++ 3_GettingCleanData/Course_Project/run_analysis.R
@@ -6,11 +6,13 @@ library(dplyr)
 #------------------------------------
 # Create and prepare folder where the data will reside
 # Make sure working directory is set to the correct  file path
+getwd()
 if (!file.exists("paste you PATH here"))
         {dir.create("paste you PATH here")}
 
 # File assignment. Make sure to go back to that file/folder and unzip file. see ReadMe file for details.
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
 #------------------------------------
 
 # ------------------------------------
